251fd7e9c030bcf109eb538451f860faf137dac3
mawonde <mawondep@gmail.com>
2023-08-16 08:02:40 -0700

    Fixed file number 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,17 +1,11 @@
 #!/usr/bin/node
 
-const args = process.argv.slice(2);
-
-if (args.length === 0) {
-  console.log("Missing number of occurrences");
+let i = parseInt(process.argv[2]);
+if (Number.isNaN(i)) {
+  console.log('Missing number of occurrences');
 } else {
-  const x = parseInt(args[0]);
-
-  if (isNaN(x)) {
-    console.log("Missing number of occurrences");
-  } else {
-    for (let i = 0; i < x; i++) {
-      console.log("C is fun");
-    }
+  while (i > 0) {
+    console.log('C is fun');
+    i--;
   }
 }
